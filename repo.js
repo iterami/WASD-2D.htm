9eef5a9ce1e6c864389fd7561136ce8136a934f1
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 15:27:20 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,34 @@
 'use strict';
 
+function load_data(id){
+    entity_create({
+      'id': 'player',
+    });
+    entity_create({
+      'id': 'target',
+    });
+
+    randomize_target();
+
+    core_ui_update({
+      'ids': {
+        'down': String.fromCharCode(core_storage_data['move-↓']),
+        'left': String.fromCharCode(core_storage_data['move-←']),
+        'right': String.fromCharCode(core_storage_data['move-→']),
+        'up': String.fromCharCode(core_storage_data['move-↑']),
+      },
+    });
+}
+
+function randomize_target(){
+    entity_entities['target']['x'] = core_random_integer({
+      'max': core_storage_data['width'],
+    }) - core_storage_data['width'] / 2;
+    entity_entities['target']['y'] = core_random_integer({
+      'max': core_storage_data['height'],
+    }) - core_storage_data['height'] / 2;
+}
+
 function repo_drawlogic(){
     canvas_buffer.save();
     canvas_buffer.translate(
